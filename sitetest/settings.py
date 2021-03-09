30d4a223c456c41b84a643465539fa69398aceff
thayer6 <casey.thayer6@gmail.com>
2021-03-09 12:58:14 -0800

    fix static path

--- sitetest/settings.py
+++ sitetest/settings.py
@@ -144,11 +144,11 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 ]
 
 # Simplified static file serving.
