9c838c1554af6d751abfd49c0c3b109c173fd4b1
thayer6 <casey.thayer6@gmail.com>
2021-03-10 16:58:19 +0000

    add images reformat html add link to app_page

--- sitetest/urls.py
+++ sitetest/urls.py
@@ -25,5 +25,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", TemplateView.as_view(template_name = "home.html"), name = "home"),
-    path("app_page/", appview) # add test page
+    path("app_page/", appview, name = "app_page") # add test page
 ]
